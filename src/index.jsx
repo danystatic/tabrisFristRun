d733a396001b0a47a70e234c155864f450497b07
Villarreal Trevino Daniel Roberto <daniel.villarreal@corp.nearshoretc.com>
2020-01-21 11:25:40 -0600

    Tabris init and webview with website

---
+++ src/index.jsx
@@ -0,0 +1,13 @@
+import { contentView, TextInput, WebView } from 'tabris';
+
+contentView.append(
+  <$>
+    <WebView stretchX bottom top='prev() 8' />
+  </$>
+);
+
+function loadUrl() {
+  $(WebView).only().url = "https://nflchallenge.club";
+}
+
+loadUrl();
